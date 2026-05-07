f92c037bc38d25f55de9f42cc8528f752c58cb39
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2026-05-07 19:11:35 +0330

    guard against missing header row pt2

--- tests/test_core.py
+++ tests/test_core.py
@@ -95,3 +95,4 @@ def test_empty_histogram_raises(collector):
 def test_snapshot_groups_by_kind(collector):
     collector.inc("hits")
     collector.gauge("temp", 3)
+    collector.observe("times", 5)
